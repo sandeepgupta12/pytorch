a2de33c768d752ad7898f4904c88e95b16c4972b
sandeepgupta12 <sandeep.gupta12@ibm.com>
2024-11-04 10:45:54 +0530

    updated dockerfile

---
+++ .github/scripts/ppc64le-build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Environment variables
+PACKAGE_NAME=pytorch
+PACKAGE_VERSION=${PACKAGE_VERSION:-v2.4.0}
+
+cd /workspace/$PACKAGE_NAME
+
+# Build and install PyTorch wheel
+if ! (MAX_JOBS=$(nproc) python setup.py bdist_wheel && pip install dist/*.whl); then
+    echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
+    exit 1
+fi
+
+# Basic test to ensure installation success
+pip install pytest
+if ! pytest test/test_utils.py; then
+    echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:install_&_test_both_success-------------------------"
+    exit 0
+fi
