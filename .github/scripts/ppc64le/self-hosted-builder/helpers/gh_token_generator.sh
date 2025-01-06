802f032f3553debbd693da4bdece98451410ec5c
sandeepgupta12 <sandeep.gupta12@ibm.com>
2025-01-06 20:47:53 +0530

    updating logic for token

--- .github/scripts/ppc64le/self-hosted-builder/helpers/gh_token_generator.sh
+++ .github/scripts/ppc64le/self-hosted-builder/helpers/gh_token_generator.sh
@@ -10,5 +10,5 @@ DST_FILE="$4"
 #echo "INSTALL_ID file content: $(<"${INSTALL_ID}")"
 #echo "APP_PRIVATE_KEY file content: $(<"${APP_PRIVATE_KEY}")"
 
-ACCESS_TOKEN="$(APP_ID="$(<"${APP_ID}")" INSTALL_ID="$(<"${INSTALL_ID}")" APP_PRIVATE_KEY="$("${APP_PRIVATE_KEY}")" "${SCRIPT_DIR}/app_token.sh")"
+ACCESS_TOKEN="$(APP_ID="$(<"${APP_ID}")" INSTALL_ID="$(<"${INSTALL_ID}")" APP_PRIVATE_KEY="${APP_PRIVATE_KEY}" "${SCRIPT_DIR}/app_token.sh")"
 echo "ACCESS_TOKEN=${ACCESS_TOKEN}" > "${DST_FILE}"
