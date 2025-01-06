ed0d056ea573a9a871972f310f66d9f5dbe36b57
sandeepgupta12 <sandeep.gupta12@ibm.com>
2025-01-06 19:21:23 +0530

    updating logic for token

--- .github/scripts/ppc64le/self-hosted-builder/helpers/app_token.sh
+++ .github/scripts/ppc64le/self-hosted-builder/helpers/app_token.sh
@@ -27,17 +27,18 @@ signature=$(echo -n "${header_base64}.${payload_base64}" | \
 
 generated_jwt="${header_base64}.${payload_base64}.${signature}"
 
-API_VERSION=v3
-API_HEADER="Accept: application/vnd.github+json"
+echo $generated_jwt
+# API_VERSION=v3
+# API_HEADER="Accept: application/vnd.github+json"
 
-auth_header="Authorization: Bearer ${generated_jwt}"
+# auth_header="Authorization: Bearer ${generated_jwt}"
 
-app_installations_response=$(curl -sX POST \
-        -H "${auth_header}" \
-        -H "${API_HEADER}" \
-        --url "https://api.github.com/app/installations/${INSTALL_ID}/access_tokens" \
-    )
+# app_installations_response=$(curl -sX POST \
+#         -H "${auth_header}" \
+#         -H "${API_HEADER}" \
+#         --url "https://api.github.com/app/installations/${INSTALL_ID}/access_tokens" \
+#     )
 
-echo "$app_installations_response" | jq --raw-output '.token'
+# echo "$app_installations_response" | jq --raw-output '.token'
 
 #echo "ACCESS_TOKEN=${jwt}" > "${DST_FILE}"
