a72aaea875b0e84197e748923026012c448a0648
sandeepgupta12 <sandeep.gupta12@ibm.com>
2025-02-05 12:54:34 +0530

    finilizing changes

--- .github/scripts/ppc64le-build.sh
+++ .github/scripts/ppc64le-build.sh
@@ -18,10 +18,7 @@ fi
 
 cd ..
 pip install pytest pytest-xdist
-if ! pytest -n $(nproc) -vvvv $PACKAGE_NAME/test/common_extended_utils.py $PACKAGE_NAME/test/common_utils.py $PACKAGE_NAME/test/smoke_test.py $PACKAGE_NAME/test/test_architecture_ops.py $PACKAGE_NAME/test/test_datasets_video_utils_opt.py $PACKAGE_NAME/test/test_tv_tensors.py; then
-   echo "------------------$PACKAGE_NAME:install_success_but_test_fails ###---------------------"
-   exit 0
-fi
+
 echo "-----start test
 if ! pytest -Xrs test; then
     echo "------------------$PACKAGE_NAME:install_success_but_test_fails---------------------"
