b65dad977057a40b58cf0cee7c01a3c41bf5ffd8
sandeepgupta12 <sandeep.gupta12@ibm.com>
2025-02-14 18:05:40 +0530

    improvise some file

--- .github/scripts/ppc64le-build.sh
+++ .github/scripts/ppc64le-build.sh
@@ -2,7 +2,7 @@
 
 # Environment variables
 PACKAGE_NAME=pytorch
-PACKAGE_VERSION=${PACKAGE_VERSION:-v2.4.0}
+PACKAGE_VERSION=${PACKAGE_VERSION:-v2.6.0}
 
 cd /workspace/$PACKAGE_NAME
 
@@ -10,7 +10,7 @@ cd /workspace/$PACKAGE_NAME
 rm -rf build/ dist/ torch.egg-info/
 
 # Build and install PyTorch wheel
-if ! (MAX_JOBS=4 python setup.py bdist_wheel && pip install dist/*.whl); then
+if ! (MAX_JOBS=$(nproc) python setup.py bdist_wheel && pip install dist/*.whl); then
     echo "------------------$PACKAGE_NAME:install_fails-------------------------------------"
     exit 1
 fi
