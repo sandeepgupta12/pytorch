b4e5d1140a54928c4ac886ed0aeca9b512323771
sandeepgupta12 <sandeep.gupta12@ibm.com>
2025-07-23 17:44:12 +0530

    using 4x runner

--- .ci/docker/manywheel/build_scripts/build.sh
+++ .ci/docker/manywheel/build_scripts/build.sh
@@ -42,34 +42,34 @@ yum -y install bzip2 make git patch unzip bison yasm diffutils \
 # If the architecture is not ppc64le, use the existing build_autoconf function
 if [ "$(uname -m)" != "ppc64le" ] ; then
     build_autoconf $AUTOCONF_ROOT $AUTOCONF_HASH
-else
-    curl -sLO http://ftp.gnu.org/gnu/autoconf/$AUTOCONF_ROOT.tar.gz
+# else
+#     curl -sLO http://ftp.gnu.org/gnu/autoconf/$AUTOCONF_ROOT.tar.gz
 
-    echo "$AUTOCONF_HASH  $AUTOCONF_ROOT.tar.gz" | sha256sum -c -
+#     echo "$AUTOCONF_HASH  $AUTOCONF_ROOT.tar.gz" | sha256sum -c -
 
-    tar -xzf $AUTOCONF_ROOT.tar.gz
-    cd $AUTOCONF_ROOT
+#     tar -xzf $AUTOCONF_ROOT.tar.gz
+#     cd $AUTOCONF_ROOT
 
-    mkdir -p build-aux
+#     mkdir -p build-aux
 
-    curl -fLo /tmp/config.guess https://git.savannah.gnu.org/cgit/config.git/plain/config.guess
-    curl -fLo /tmp/config.sub https://git.savannah.gnu.org/cgit/config.git/plain/config.sub
+#     curl -fLo /tmp/config.guess https://git.savannah.gnu.org/cgit/config.git/plain/config.guess
+#     curl -fLo /tmp/config.sub https://git.savannah.gnu.org/cgit/config.git/plain/config.sub
 
-    chmod +x /tmp/config.guess /tmp/config.sub
+#     chmod +x /tmp/config.guess /tmp/config.sub
 
-    mv /tmp/config.guess build-aux/config.guess
-    mv /tmp/config.sub build-aux/config.sub
+#     mv /tmp/config.guess build-aux/config.guess
+#     mv /tmp/config.sub build-aux/config.sub
 
-    ls -lh build-aux/config.*
+#     ls -lh build-aux/config.*
 
-    ./build-aux/config.guess || echo "Failed to detect architecture"
+#     ./build-aux/config.guess || echo "Failed to detect architecture"
 
-    ./configure --host=powerpc64le-pc-linux-gnu
-    make -j$(nproc)
-    make install
+#     ./configure --host=powerpc64le-pc-linux-gnu
+#     make -j$(nproc)
+#     make install
 
-    cd ..
-    rm -rf $AUTOCONF_ROOT $AUTOCONF_ROOT.tar.gz
+#     cd ..
+#     rm -rf $AUTOCONF_ROOT $AUTOCONF_ROOT.tar.gz
 fi
 autoconf --version
 
