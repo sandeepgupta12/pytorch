45eb321bcdbe5aa78dbe8ff59e2a0111ef2f8d70
sandeepgupta12 <sandeep.gupta12@ibm.com>
2026-02-02 15:08:35 +0530

    using pipe logic 3

--- .github/scripts/ppc64le-ci/self-hosted-builder/helpers/gh_cat_token.sh
+++ .github/scripts/ppc64le-ci/self-hosted-builder/helpers/gh_cat_token.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 TOKEN_FILE=$1
-OUTPUT_FILE=$2
+TOKEN_PIPE=$2
 
-echo "Starting gh_cat_token.sh with TOKEN_FILE=${TOKEN_FILE}, OUTPUT_FILE=${OUTPUT_FILE}"
+echo "Starting gh_cat_token.sh with TOKEN_FILE=${TOKEN_FILE}, TOKEN_PIPE=${TOKEN_PIPE}"
 
 # Validate inputs
 if [[ ! -r "${TOKEN_FILE}" ]]; then
